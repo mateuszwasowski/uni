305ef5d33973e03a4f5cd4d67bb5e7b345d65f81
MateuszWasowski <wasowski.mateusz@gmail.com>
2016-03-18 22:30:18 +0100

    test

--- app/controllers/omniauth_callback_controller.rb
+++ app/controllers/omniauth_callback_controller.rb
@@ -1,32 +1,19 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
-    generic_callback( 'facebook' )
-  end
+      # You need to implement the method below in your model (e.g. app/models/user.rb)
+      @user = User.from_omniauth(request.env["omniauth.auth"])
 
-  def generic_callback( provider )
-    @identity = Identity.find_for_oauth env["omniauth.auth"]
-
-    @user = @identity.user || current_user
-    if @user.nil?
-      @user = User.create( email: @identity.email || "" )
-      @identity.update_attribute( :user_id, @user.id )
+      if @user.persisted?
+        sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+        set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      else
+        session["devise.facebook_data"] = request.env["omniauth.auth"]
+        redirect_to new_user_registration_url
+      end
     end
 
-    if @user.email.blank? && @identity.email
-      @user.update_attribute( :email, @identity.email)
+    def failure
+      redirect_to root_path
     end
-
-    if @user.persisted?
-      @identity.update_attribute( :user_id, @user.id )
-      # This is because we've created the user manually, and Device expects a
-      # FormUser class (with the validations)
-      @user = FormUser.find @user.id
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: provider.capitalize) if is_navigational_format?
-    else
-      session["devise.#{provider}_data"] = env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
+  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks'}
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
