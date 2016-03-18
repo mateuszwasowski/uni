121829a0d3ae741c8f4c52ea5996a64f802fe5ca
MateuszWasowski <wasowski.mateusz@gmail.com>
2016-03-18 18:19:42 +0100

    add login route, precompile assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  resources :home, only: [:index]
+  resources :home, only: [:index] do
 
+  end
+  get 'login', to: "home#login", as: :login
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
