4146939764a80af0ffc392bd30d72f6ee6a768bc
MateuszWasowski <wasowski.mateusz@gmail.com>
2016-03-18 18:01:55 +0100

    add js and icons + modify route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :home
+  root to: 'home#index'
+  resources :home, only: [:index]
 
 
   # The priority is based upon order of creation: first created -> highest priority.
