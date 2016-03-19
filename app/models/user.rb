7cff4216e72762bb922e2eefcbeb730875911c12
MateuszWasowski <wasowski.mateusz@gmail.com>
2016-03-19 10:59:35 +0100

    fix?

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :registerable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
   devise :omniauthable, :omniauth_providers => [:facebook]
   mount_uploader :avatar, AvatarUploader
   has_and_belongs_to_many :universities
