00338c1bff57ebc9217d5fc44ae8ad17418d1cc2
MateuszWasowski <wasowski.mateusz@gmail.com>
2016-03-19 12:11:41 +0100

    special folders

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
     get 'speciality/:id', to: "catalog#speciality", as: :speciality
   end
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
