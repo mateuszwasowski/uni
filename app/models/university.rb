77a8b096ba509d8fa109dfc747264ff212bd7112
MateuszWasowski <wasowski.mateusz@gmail.com>
2016-03-19 10:46:37 +0100

    uni

--- app/models/university.rb
+++ app/models/university.rb
@@ -1,6 +1,6 @@
 class University < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
-  validates_presence_of :name, :avatar
+  validates_presence_of :name
   has_and_belongs_to_many :users
   has_many :catalogs
 end
