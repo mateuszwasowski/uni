bf0e6685bd47f27b4671520316fd00791a5c2719
MateuszWasowski <wasowski.mateusz@gmail.com>
2016-03-19 10:42:56 +0100

    current user universities

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,10 @@ class HomeController < ApplicationController
 #   before_action :authenticate_user!, except: [:index]
 
   def home
-    @uni = University.all
-
+    if current_user
+      @uni = current_user.universities
+    else
+      @uni = University.all
+    end
   end
 end
